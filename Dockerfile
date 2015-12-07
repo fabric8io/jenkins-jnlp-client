60f4db47e7b138a0c7d32f74a2d459dfa8925fcd
Ioannis Canellos <iocanel@gmail.com>
2015-12-07 11:20:56 +0200

    All mounts are now moved under /home/jenkins

--- Dockerfile
+++ Dockerfile
@@ -20,15 +20,14 @@ RUN cd /usr/local && \
     rm /usr/local/hub-linux-amd64-2.2.1.tar.gz && \
     ln -s /usr/local/hub-linux-amd64-2.2.1/hub /usr/bin/hub
 
-RUN mkdir -p /root/.m2/
-COPY mvnsettings.xml /root/.m2/settings.xml
-COPY ssh-config /root/.ssh/config
+RUN mkdir -p /home/jenkins/.m2/
+COPY mvnsettings.xml /home/jenkins/.m2/settings.xml
+COPY ssh-config /home/jenkins/.ssh/config
 
 ENV JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64/
-ENV JENKINS_HOME /var/jenkins
+ENV JENKINS_HOME /home/jenkins
 
 COPY start.sh /usr/local/bin/start.sh
-
-WORKDIR /var/jenkins
+WORKDIR /home/jenkins
 
 ENTRYPOINT ["/usr/local/bin/start.sh"]
--- start.sh
+++ start.sh
@@ -4,16 +4,16 @@
 # add lots of error handling.  If this script fails it's hard to know why the pods keeps restarting
 # ***** IMPORTANT *****
 
-if [ -d "/var/jenkins/.ssh-git" ]; then
-	chmod 600 /var/jenkins/.ssh-git/ssh-key
-	chmod 600 /var/jenkins/.ssh-git/ssh-key.pub
-	chmod 700 /var/jenkins/.ssh-git
+if [ -d "/home/jenkins/.ssh-git" ]; then
+	chmod 600 /home/jenkins/.ssh-git/ssh-key
+	chmod 600 /home/jenkins/.ssh-git/ssh-key.pub
+	chmod 700 /home/jenkins/.ssh-git
 fi
-if [ -d "/root/.gnupg" ]; then
-	chmod 600 /root/.gnupg/pubring.gpg
-	chmod 600 /root/.gnupg/secring.gpg
-	chmod 600 /root/.gnupg/trustdb.gpg
-	chmod 700 /root/.gnupg
+if [ -d "/home/jenkins/.gnupg" ]; then
+	chmod 600 /home/jenkins/.gnupg/pubring.gpg
+	chmod 600 /home/jenkins/.gnupg/secring.gpg
+	chmod 600 /home/jenkins/.gnupg/trustdb.gpg
+	chmod 700 /home/jenkins/.gnupg
 fi
 
 if [ "$DOCKER_REGISTRY_SERVER_ID" = "docker.io" ]; then
